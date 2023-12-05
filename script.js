f13c69ca4e579d6d6692d1d97f59a7a8065a3d67
Mead Solomon <mead93@gmail.com>
2023-12-04 22:05:21 -0600

    added floor, animation, and camera

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
-console.log("JavaScript is working");
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const floorGeometry = new THREE.PlaneGeometry(1, 1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0x808080,
+});
+const mesh = new THREE.Mesh(floorGeometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  1,
+  1000
+);
+camera.position.x = 3;
+camera.position.y = 3;
+camera.position.z = 3;
+camera.lookAt(mesh.position);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
+
+// Animate
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
